73ecdd69094ddb202d7e87ee3de32905dfe80a95
AuGusto <oscaralock@gmail.com>
2018-02-11 14:02:34 -0800

    2/4/18
    All edited within MapsActivity.kt
    
    -Removed initial introductory page.
    
    -Created Google Maps integration with location specification (Sacramento State)
    
    -Created buttons to move throughout the wireframe.
    
    2/11/18
    
    Login.kt
    
    -Created front end for the ability to input login and password.
    
    -Created buttons to reroute and accept login to grant administrator privileges.

--- app/src/main/java/edu/csus/ecs/athena/meeseeksparking/Login.kt
+++ app/src/main/java/edu/csus/ecs/athena/meeseeksparking/Login.kt
@@ -2,11 +2,26 @@ package edu.csus.ecs.athena.meeseeksparking
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button5.setOnClickListener{
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        button6.setOnClickListener{
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
