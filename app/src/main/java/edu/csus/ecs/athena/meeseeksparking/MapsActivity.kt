58844f000e70044f4dbfdd9a63cc36445c93a589
Jenny <jenjenyaoyao@gmail.com>
2018-02-25 13:04:31 -0800

    created the UI for the overview.xml and added some strings

--- app/src/main/java/edu/csus/ecs/athena/meeseeksparking/MapsActivity.kt
+++ app/src/main/java/edu/csus/ecs/athena/meeseeksparking/MapsActivity.kt
@@ -11,7 +11,6 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 
-
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
@@ -38,12 +37,77 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             val intent = Intent(this, Overview::class.java)
             startActivity(intent)
         }
-
-
-
-
     }
 
+//    override fun drawPolygon(googleMap: GoogleMap) {
+//
+//        val lot5 = googleMap.addPolygon(PolygonOptions()
+//                .clickable(true)
+//                .add(
+//                        LatLng(38.558605, -121.422440),
+//                        LatLng(38.558785, -121.422515),
+//                        LatLng(38.558897, -121.422112),
+//                        LatLng(38.558716, -121.422042)))
+////        fillColor(0x330000FF)
+////        polygon1.setTag("alpha")
+//
+//        val lot7 = googleMap.addPolygon(PolygonOptions()
+//                .clickable(true)
+//                .add(
+//                        LatLng(38.554225, -121.418572),
+//                        LatLng(38.554273, -121.419824),
+//                        LatLng(38.557206, -121.420485),
+//                        LatLng(38.557339, -121.420280),
+//                        LatLng(38.556935, -121.419811),
+//                        LatLng(38.557265, -121.418599)))
+////        fillColor(0x330000FF)
+////        polygon1.setTag("alpha")
+//
+//        val ps1 = googleMap.addPolygon(PolygonOptions()
+//                .clickable(true)
+//                .add(
+//                        LatLng(38.559075, -121.427242),
+//                        LatLng(38.559650, -121.427715),
+//                        LatLng(38.560192, -121.426635),
+//                        LatLng(38.560182, -121.426544),
+//                        LatLng(38.559619, -121.426090)))
+////        fillColor(0x330000FF)
+////        polygon1.setTag("alpha")
+//
+//        val ps2 = googleMap.addPolygon(PolygonOptions()
+//                .clickable(true)
+//                .add(
+//                        LatLng(38.559409, -121.420965),
+//                        LatLng(38.559650, -121.420015),
+//                        LatLng(38.559111, -121.419266),
+//                        LatLng(38.558766, -121.420606),
+//                        LatLng(38.558770, -121.420677),
+//                        LatLng(38.558817, -121.420693),
+//                        LatLng(38.558811, -121.420714)))
+////        fillColor(0x330000FF)
+////        polygon1.setTag("alpha")
+//
+//        val ps3 = googleMap.addPolygon(PolygonOptions()
+//                .clickable(true)
+//                .add(
+//                        LatLng(38.557533, -121.422433),
+//                        LatLng(38.557571, -121.422255),
+//                        LatLng(38.557627, -121.422276),
+//                        LatLng(38.557683, -121.422086),
+//                        LatLng(38.557624, -121.422071),
+//                        LatLng(38.557863, -121.421094),
+//                        LatLng(38.557808, -121.421146),
+//                        LatLng(38.557674, -121.420803),
+//                        LatLng(38.556670, -121.420361),
+//                        LatLng(38.556625, -121.420520),
+//                        LatLng(38.556706, -121.420557),
+//                        LatLng(38.556643, -121.422073),
+//                        LatLng(38.556632, -121.422149),
+//                        LatLng(38.556730, -121.422195),
+//                        LatLng(38.556746, -121.422120)))
+////        fillColor(0x330000FF)
+////        polygon1.setTag("alpha")
+//    }
 
 
     /**
