36cafdf31d9e75c48117e43d923068cfc72fc2d8
AuGusto <oscaralock@gmail.com>
2018-03-01 12:41:25 -0800

    2/4/18
    All edited within MapsActivity.kt
    
    -Removed initial introductory page.
    
    -Created Google Maps integration with location specification (Sacramento State)
    
    -Created buttons to move throughout the wireframe.
    
    2/11/18
    
    Login.kt
    
    -Created front end for the ability to input login and password.
    
    -Created buttons to reroute and accept login to grant administrator privileges.
    
    2/21/18
    
    Created Overlays for 5 seperate parking locations on campus.
    
    3/1/2018
    
    Created button fix.
    
    Implemented color coated parking structures dependent of percent occupancy.

--- app/src/main/java/edu/csus/ecs/athena/meeseeksparking/MapsActivity.kt
+++ app/src/main/java/edu/csus/ecs/athena/meeseeksparking/MapsActivity.kt
@@ -15,11 +15,18 @@ import com.google.android.gms.common.api.GoogleApiClient
 //import com.google.android.gms.location.LocationRequest
 //import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.*
+import java.util.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var myMap: GoogleMap
+
+    val a: Int = 84
+    val b: Int = 100
+    val c: Int = 25
+    val d: Int = 40
+    val e: Int = 75
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -78,86 +85,298 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         myMap = googleMap
 
+        // val a: Int = 80
+        //val b: Int = 100
+
         // Add a marker in Sydney and move the camera
         val SacState = LatLng(38.5611, -121.4240)
         myMap.addMarker(MarkerOptions().position(SacState).title("Sacramento State"))
         myMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SacState, 15.5f))
 
-        myMap.addPolygon(PolygonOptions()
-                .clickable(true)
-                .add(
-                        LatLng(38.558605, -121.422440),
-                        LatLng( 38.558785, -121.422515),
-                        LatLng( 38.558897, -121.422112),
-                        LatLng(38.558716, -121.422042))
-                .fillColor(0x33006400)
-                .strokeWidth(0F)
-                //.t
-        )
 
-        myMap.addPolygon(PolygonOptions()
-                .clickable(true)
-                .add(
-                        LatLng(38.554225, -121.418572),
-                        LatLng(38.554273, -121.419824),
-                        LatLng(38.557206, -121.420485),
-                        LatLng(38.557339, -121.420280),
-                        LatLng(38.556935, -121.419811),
-                        LatLng(38.557265, -121.418599))
-                .fillColor(0x33006400)
-                .strokeWidth(0F)
-                //.setTag("PL5")
-        )
+        if (a > 90 && a <= 100) {
 
-        myMap.addPolygon(PolygonOptions()
-                .clickable(true)
-                .add(
-                        LatLng(38.559075, -121.427242),
-                        LatLng(38.559650, -121.427715),
-                        LatLng(38.560192, -121.426635),
-                        LatLng(38.560182, -121.426544),
-                        LatLng(38.559619, -121.426090))
-                .fillColor(0x330000FF)
-                .strokeWidth(0F)
-                //.setTag("PL5")
-        )
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.558605, -121.422440),
+                            LatLng(38.558785, -121.422515),
+                            LatLng(38.558897, -121.422112),
+                            LatLng(38.558716, -121.422042))
+                    .fillColor(0x33990000)
+                    .strokeWidth(0.75F)
+                    //.t
+            )
 
-        myMap.addPolygon(PolygonOptions()
-                .clickable(true)
-                .add(
-                        LatLng(38.559409, -121.420965),
-                        LatLng(38.559650, -121.420015),
-                        LatLng(38.559111, -121.419266),
-                        LatLng(38.558766, -121.420606),
-                        LatLng(38.558770, -121.420677),
-                        LatLng(38.558817, -121.420693),
-                        LatLng(38.558811, -121.420714))
-                .fillColor(0x330000FF)
-                .strokeWidth(0F)
-                //.setTag("PL5")
-        )
+        } else if(a >= 0 && a <= 80) {
 
-        myMap.addPolygon(PolygonOptions()
-                .clickable(true)
-                .add(
-                        LatLng(38.557533, -121.422433),
-                        LatLng(38.557571, -121.422255),
-                        LatLng(38.557627, -121.422276),
-                        LatLng(38.557683, -121.422086),
-                        LatLng(38.557624, -121.422071),
-                        LatLng(38.557863, -121.421094),
-                        LatLng(38.557808, -121.421146),
-                        LatLng(38.557674, -121.420803),
-                        LatLng(38.556670, -121.420361),
-                        LatLng(38.556625, -121.420520),
-                        LatLng(38.556706, -121.420557),
-                        LatLng(38.556643, -121.422073),
-                        LatLng(38.556632, -121.422149),
-                        LatLng(38.556730, -121.422195),
-                        LatLng(38.556746, -121.422120))
-                .fillColor(0x330000FF)
-                .strokeWidth(0F)
-                //.setTag("PL5")
-        )
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.558605, -121.422440),
+                            LatLng(38.558785, -121.422515),
+                            LatLng(38.558897, -121.422112),
+                            LatLng(38.558716, -121.422042))
+                    .fillColor(0x33009900)
+                    .strokeWidth(0.75F)
+                    //.t
+            )
+
+        }
+
+        else if(a > 80 && a <= 90) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.558605, -121.422440),
+                            LatLng(38.558785, -121.422515),
+                            LatLng(38.558897, -121.422112),
+                            LatLng(38.558716, -121.422042))
+                    .fillColor(0x33FFFF00)
+                    .strokeWidth(0.75F)
+                    //.t
+            )
+
+        }
+
+        if (b > 90 && b <= 100) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.554225, -121.418572),
+                            LatLng(38.554273, -121.419824),
+                            LatLng(38.557206, -121.420485),
+                            LatLng(38.557339, -121.420280),
+                            LatLng(38.556935, -121.419811),
+                            LatLng(38.557265, -121.418599))
+                    .fillColor(0x33990000)
+                    .strokeWidth(0.75F)
+            )
+
+        } else if(b >=0  && b <= 80) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.554225, -121.418572),
+                            LatLng(38.554273, -121.419824),
+                            LatLng(38.557206, -121.420485),
+                            LatLng(38.557339, -121.420280),
+                            LatLng(38.556935, -121.419811),
+                            LatLng(38.557265, -121.418599))
+                    .fillColor(0x33009900)
+                    .strokeWidth(0.75F)
+            )
+
+        }
+
+        else if(b > 80  && b <= 90) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.554225, -121.418572),
+                            LatLng(38.554273, -121.419824),
+                            LatLng(38.557206, -121.420485),
+                            LatLng(38.557339, -121.420280),
+                            LatLng(38.556935, -121.419811),
+                            LatLng(38.557265, -121.418599))
+                    .fillColor(0x33FFFF00)
+                    .strokeWidth(0.75F)
+            )
+
+        }
+
+
+        if (c > 90 && c <= 100) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.559075, -121.427242),
+                            LatLng(38.559650, -121.427715),
+                            LatLng(38.560192, -121.426635),
+                            LatLng(38.560182, -121.426544),
+                            LatLng(38.559619, -121.426090))
+                    .fillColor(0x33990000)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        } else  if(c >= 0 && c <= 80) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.559075, -121.427242),
+                            LatLng(38.559650, -121.427715),
+                            LatLng(38.560192, -121.426635),
+                            LatLng(38.560182, -121.426544),
+                            LatLng(38.559619, -121.426090))
+                    .fillColor(0x33009900)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        }
+        else  if(c > 80  && c <= 90) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.559075, -121.427242),
+                            LatLng(38.559650, -121.427715),
+                            LatLng(38.560192, -121.426635),
+                            LatLng(38.560182, -121.426544),
+                            LatLng(38.559619, -121.426090))
+                    .fillColor(0x33FFFF00)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        }
+
+        if (d > 90 && d <= 100) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.559409, -121.420965),
+                            LatLng(38.559650, -121.420015),
+                            LatLng(38.559111, -121.419266),
+                            LatLng(38.558766, -121.420606),
+                            LatLng(38.558770, -121.420677),
+                            LatLng(38.558817, -121.420693),
+                            LatLng(38.558811, -121.420714))
+                    .fillColor(0x33990000)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        } else if(d >= 0 && d <= 80) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.559409, -121.420965),
+                            LatLng(38.559650, -121.420015),
+                            LatLng(38.559111, -121.419266),
+                            LatLng(38.558766, -121.420606),
+                            LatLng(38.558770, -121.420677),
+                            LatLng(38.558817, -121.420693),
+                            LatLng(38.558811, -121.420714))
+                    .fillColor(0x33009900)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        }
+
+        else if(d > 80 && d <= 90) {
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.559409, -121.420965),
+                            LatLng(38.559650, -121.420015),
+                            LatLng(38.559111, -121.419266),
+                            LatLng(38.558766, -121.420606),
+                            LatLng(38.558770, -121.420677),
+                            LatLng(38.558817, -121.420693),
+                            LatLng(38.558811, -121.420714))
+                    .fillColor(0x33FFFF00)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        }
+
+
+        if (e > 90 && e <= 100) {
+
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.557533, -121.422433),
+                            LatLng(38.557571, -121.422255),
+                            LatLng(38.557627, -121.422276),
+                            LatLng(38.557683, -121.422086),
+                            LatLng(38.557624, -121.422071),
+                            LatLng(38.557863, -121.421094),
+                            LatLng(38.557808, -121.421146),
+                            LatLng(38.557674, -121.420803),
+                            LatLng(38.556670, -121.420361),
+                            LatLng(38.556625, -121.420520),
+                            LatLng(38.556706, -121.420557),
+                            LatLng(38.556643, -121.422073),
+                            LatLng(38.556632, -121.422149),
+                            LatLng(38.556730, -121.422195),
+                            LatLng(38.556746, -121.422120))
+                    .fillColor(0x33990000)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        } else if(e >= 0 && e <= 80) {
+
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.557533, -121.422433),
+                            LatLng(38.557571, -121.422255),
+                            LatLng(38.557627, -121.422276),
+                            LatLng(38.557683, -121.422086),
+                            LatLng(38.557624, -121.422071),
+                            LatLng(38.557863, -121.421094),
+                            LatLng(38.557808, -121.421146),
+                            LatLng(38.557674, -121.420803),
+                            LatLng(38.556670, -121.420361),
+                            LatLng(38.556625, -121.420520),
+                            LatLng(38.556706, -121.420557),
+                            LatLng(38.556643, -121.422073),
+                            LatLng(38.556632, -121.422149),
+                            LatLng(38.556730, -121.422195),
+                            LatLng(38.556746, -121.422120))
+                    .fillColor(0x33009900)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        }
+
+        else if(e > 80 && e <= 90) {
+
+
+            myMap.addPolygon(PolygonOptions()
+                    .clickable(true)
+                    .add(
+                            LatLng(38.557533, -121.422433),
+                            LatLng(38.557571, -121.422255),
+                            LatLng(38.557627, -121.422276),
+                            LatLng(38.557683, -121.422086),
+                            LatLng(38.557624, -121.422071),
+                            LatLng(38.557863, -121.421094),
+                            LatLng(38.557808, -121.421146),
+                            LatLng(38.557674, -121.420803),
+                            LatLng(38.556670, -121.420361),
+                            LatLng(38.556625, -121.420520),
+                            LatLng(38.556706, -121.420557),
+                            LatLng(38.556643, -121.422073),
+                            LatLng(38.556632, -121.422149),
+                            LatLng(38.556730, -121.422195),
+                            LatLng(38.556746, -121.422120))
+                    .fillColor(0x33FFFF00)
+                    .strokeWidth(0.75F)
+                    //.setTag("PL5")
+            )
+
+        }
+
+
     }
 }
+
