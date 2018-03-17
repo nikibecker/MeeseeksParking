8771aa9e573b852589d482e101e1eaf40a9b2571
AuGusto <oscaralock@gmail.com>
2018-03-16 20:37:41 -0700

    2/4/18
    All edited within MapsActivity.kt
    
    -Removed initial introductory page.
    
    -Created Google Maps integration with location specification (Sacramento State)
    
    -Created buttons to move throughout the wireframe.
    
    2/11/18
    
    Login.kt
    
    -Created front end for the ability to input login and password.
    
    -Created buttons to reroute and accept login to grant administrator privileges.
    
    2/21/18
    
    Created Overlays for 5 seperate parking locations on campus.
    
    3/1/2018
    
    Created button fix.
    
    Implemented color coated parking structures dependent of percent occupancy.

--- app/src/main/java/edu/csus/ecs/athena/meeseeksparking/Overview.kt
+++ app/src/main/java/edu/csus/ecs/athena/meeseeksparking/Overview.kt
@@ -4,7 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-
+import kotlinx.android.synthetic.main.activity_maps.*
+import kotlinx.android.synthetic.main.activity_overview.*
 
 
 class Overview : AppCompatActivity() {
@@ -12,11 +13,36 @@ class Overview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overview)
+
+        button8.setOnClickListener{
+            val intent = Intent(this, lot5::class.java)
+            startActivity(intent)
+        }
+
+        button9.setOnClickListener{
+            val intent = Intent(this, lot7::class.java)
+            startActivity(intent)
+        }
+
+        button10.setOnClickListener{
+            val intent = Intent(this, ps1::class.java)
+            startActivity(intent)
+        }
+        button12.setOnClickListener{
+            val intent = Intent(this, ps2::class.java)
+            startActivity(intent)
+        }
+        button11.setOnClickListener{
+            val intent = Intent(this, ps3::class.java)
+            startActivity(intent)
+        }
+
     }
     fun goToOverview(view: View) {
         val intent = Intent(this, MapsActivity::class.java)
         startActivity(intent)
     }
+    /*
     fun goToActivityLot5(view: View) {
         val intent = Intent(this, MapLot5::class.java)
         startActivity(intent)
@@ -37,4 +63,5 @@ class Overview : AppCompatActivity() {
         val intent = Intent(this, MapPS3::class.java)
         startActivity(intent)
     }
+    */
 }
