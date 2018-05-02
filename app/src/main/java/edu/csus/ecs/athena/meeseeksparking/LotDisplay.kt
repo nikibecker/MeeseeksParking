2d9452e2ecc0bcc4c1bf6f6123eae57bca6cf771
AuGusto <oscaralock@gmail.com>
2018-05-02 16:12:47 -0700

    2/4/18
    All edited within MapsActivity.kt
    
    -Removed initial introductory page.
    
    -Created Google Maps integration with location specification (Sacramento State)
    
    -Created buttons to move throughout the wireframe.
    
    2/11/18
    
    Login.kt
    
    -Created front end for the ability to input login and password.
    
    -Created buttons to reroute and accept login to grant administrator privileges.
    
    2/21/18
    
    Created Overlays for 5 seperate parking locations on campus.
    
    3/1/2018
    
    Created button fix.
    
    Implemented color coated parking structures dependent of percent occupancy.

--- app/src/main/java/edu/csus/ecs/athena/meeseeksparking/LotDisplay.kt
+++ app/src/main/java/edu/csus/ecs/athena/meeseeksparking/LotDisplay.kt
@@ -4,22 +4,34 @@
 package edu.csus.ecs.athena.meeseeksparking
 
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.HorizontalScrollView
 import android.widget.LinearLayout
+import java.sql.Blob
+import java.sql.ResultSet
+import android.graphics.Bitmap
+
+
 
 class LotDisplay : AppCompatActivity() {
+
+    var LotNameString : String = "lot1" //pushed from the previous activity
+    var FloorNumInt : Int = 0 //pushed from the previous activity
+    var conf: Bitmap.Config = Bitmap.Config.ARGB_8888 // see other conf types
+    var bmp = Bitmap.createBitmap(0, 0, conf)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lot_display)
+
+        LotNameString = "GetNameFromActivityPush" //TODO retrieve correct name
+        FloorNumInt = 1 //TODO retreive correct floor
+
+        getMapBmp()
 
         val scrollyBoi = findViewById<HorizontalScrollView>(R.id.scrollyBoi) as LinearLayout
         val background = Canvass(this)
@@ -28,28 +40,23 @@ class LotDisplay : AppCompatActivity() {
 
     inner class Canvass(context: Context) : View(context) {
 
-        val bmp = BitmapFactory.decodeResource(resources, R.mipmap.lot3)
+        //val bmp = BitmapFactory.decodeResource(resources, R.mipmap.lot3)
+
         val red = Paint()
         init {
             red.color = Color.RED
             red.style = Paint.Style.FILL
-        }
-        val rededge = Paint()
-        init {
-            rededge.color = Color.BLACK
-            rededge.style = Paint.Style.STROKE
-            rededge.strokeWidth = 3.5f
         }
         val green = Paint()
         init {
             green.style = Paint.Style.FILL
             green.color = Color.GREEN
         }
-        val greenedge = Paint()
+        val edge = Paint()
         init {
-            greenedge.color = Color.BLACK
-            greenedge.style = Paint.Style.STROKE
-            greenedge.strokeWidth = 3.5F
+           edge.color = Color.BLACK
+            edge.style = Paint.Style.STROKE
+            edge.strokeWidth = 3.5F
         }
 
         init {
@@ -58,45 +65,51 @@ class LotDisplay : AppCompatActivity() {
 
         override fun onDraw(canvas: Canvas) {
             canvas.drawRGB(220, 220, 220)
-/*
-            var sqlQueryStr = "SELECT Poly, Available FROM parkingspot WHERE LotName = ? AND FloorNum = ?"
-            var querySQL = QuerySQL()
-            var results : ResultSet = querySQL.execute(sqlQueryStr,LotNameString, FloorNumInt)
-            if(results.next()) {
-                var avail = results.getInt("Available")
-                var poly = results.getString("Poly")
-            }
-            querySQL.close() */
 
             canvas.drawBitmap(bmp,0f,0f, null)
+
+            var sqlQueryPoly = "SELECT Left, Right, Top, Bottom, DegRotation, Available FROM parkingspot WHERE LotName = ? AND FloorNum = ?"
+            var querySQL = QuerySQL()
+            var results : ResultSet = querySQL.execute(sqlQueryPoly,LotNameString, FloorNumInt)
+            while(results.next()) {
+                var left : Float = results.getFloat("Left")
+                var right  : Float = results.getFloat("Right")
+                var top  : Float = results.getFloat("Top")
+                var bottom : Float = results.getFloat("Bottom")
+                var degRotation : Float = results.getFloat("DegRotation")
+                var avail : Int = results.getInt("Available")
+
+                canvas.save()
+                canvas.rotate(degRotation, (left+right)/2, (top+bottom)/2)
+                if (avail == 1)
+                    canvas.drawRect(left, top, right, bottom, green)
+
+                else
+                    canvas.drawRect(left, top, right, bottom, red)
+
+                canvas.drawRect(left, top, right, bottom, edge)
+                canvas.restore()
+            }
+            querySQL.close()
+
 
             //canvas.save()
            // canvas.rotate(45f, 105f, 1100f)
             //canvas.drawRect(10f, 1000f, 200f, 1200f, red)
             //canvas.restore()
-
-            canvas.save()
-
-            canvas.drawRect(90f, 850f, 150f, 990f, green)
-            canvas.drawRect(90f, 850f, 150f, 990f, greenedge)
-
-            canvas.restore()
-            // What is going to be used in order to pull information from the database.
-
-            canvas.save()
-            canvas.rotate(0f, 0f, 0f)
-            canvas.drawRect(160f, 850f, 220f, 990f, red)
-            canvas.drawRect(160f, 850f, 220f, 990f, rededge)
-            canvas.restore()
-
-            canvas.save()
-            canvas.rotate(0f, 0f, 0f)
-            canvas.drawRect(230f, 850f, 290f, 990f, red)
-            canvas.drawRect(230f, 850f, 290f, 990f, rededge)
-
-            canvas.restore()
-
-
         }
+    }
+
+    private fun getMapBmp() : Bitmap{
+        var sqlQueryStr = "SELECT lotMap FROM parkinglot WHERE LotName = ? AND FloorNum = ?"
+        var querySQL = QuerySQL()
+        var results : ResultSet = querySQL.execute(sqlQueryStr,LotNameString, FloorNumInt)
+        if(results.next()) {
+            var blob : Blob = results.getBlob("lotMap")
+            var mapBytes = blob.getBytes(1, blob.length().toInt())
+            bmp = BitmapFactory.decodeByteArray(mapBytes,0, blob.length().toInt())
+        }
+        querySQL.close()
+        return bmp
     }
 }
