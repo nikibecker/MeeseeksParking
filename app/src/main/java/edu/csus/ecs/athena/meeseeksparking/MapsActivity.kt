38c7524c3797854512c2bb76014723943a93033f
Nicole <nicolebecker@csus.edu>
2018-05-03 14:34:59 -0700

    Finished dynamic lots for main map page. They now send correct data to individual lot page.

--- app/src/main/java/edu/csus/ecs/athena/meeseeksparking/MapsActivity.kt
+++ app/src/main/java/edu/csus/ecs/athena/meeseeksparking/MapsActivity.kt
@@ -3,15 +3,11 @@ package edu.csus.ecs.athena.meeseeksparking
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import android.widget.EditText
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_maps.*
-//import com.google.android.gms.location.LocationListener
-//import com.google.android.gms.location.LocationRequest
-//import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.*
 import java.sql.ResultSet
 import java.util.*
@@ -21,17 +17,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var myMap: GoogleMap
     private lateinit var myMap1: GoogleMap
-    private lateinit var myMap2: GoogleMap
-    private lateinit var myMap3: GoogleMap
-    private lateinit var myMap4: GoogleMap
-
-    //False Variables to test for Parking Structure Color Representations
-
-    val a: Int = 95
-    val b: Int = 100
-    val c: Int = 25
-    val d: Int = 40
-    val e: Int = 75
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -82,8 +67,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val spotCounts : MutableList<Int> = ArrayList()
         val spotsTaken : MutableList<Int> = ArrayList()
         val polys : MutableList<String> = ArrayList()
-        val lots : MutableMap<Int, Polygon>
-        val names : MutableMap<Int, String>
 
         while (results.next())
         {
@@ -99,9 +82,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         //Instantiate the Map for Google
         myMap = googleMap
         myMap1 = googleMap
-        //myMap2 = googleMap
-        //myMap3 = googleMap
-        //myMap4 = googleMap
 
         // Add a marker in Sacramento State and zoom in to view the campus positions well
         val SacState = LatLng(38.5611, -121.4240)
@@ -109,7 +89,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         myMap1.moveCamera(CameraUpdateFactory.newLatLngZoom(SacState, 15.5f))
 
         //Start code for dynamic polygon creation
-        //val map = mutableMapOf<String, Int>()
         var i = 0
 
         for (name in lotNames) {
@@ -120,6 +99,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 val y = xys.substringAfter(" ").toDouble()
                 latLongs.add(LatLng(x, y))
             }
+
             var color = 0x33FFFF00
             val a = spotsTaken.get(i)/spotCounts.get(i)
             if (a > .90 && a <= 1.00)
@@ -142,8 +122,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         myMap.setOnPolygonClickListener {
             val intentLotDisplay = Intent(this, LotDisplay::class.java)
-            val lotName = "lot3" //need to make dynamic
-
+            val ind = it.zIndex
+            val lotName = lotNames.get(ind.toInt())
+            System.out.println(lotName)
             intentLotDisplay.putExtra("lotNameData", lotName)
             intentLotDisplay.putExtra("floorNumData", 1)
             startActivity(intentLotDisplay)
